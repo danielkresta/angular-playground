c0147b3bc909a7b34b9520b5a3ea2569ffa8ca39
mgmarlow <Graham.Marlow@socreate.it>
2018-01-09 10:04:36 -0800

    nest cli and core under same package name

---
+++ core/src/playground-common.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { CommonModule, Location, LocationStrategy, PathLocationStrategy } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ScenarioComponent } from './scenario/scenario.component';
+import { StateService } from './shared/state.service';
+import { FocusDirective } from './shared/focus.directive';
+import { UrlService } from './shared/url.service';
+import { AppComponent } from './app.component';
+import { LevenshteinDistance } from './shared/levenshtein-distance';
+import { HighlightSearchMatchPipe } from './shared/highlight-search-match.pipe';
+import { LoaderService } from './shared/loader.service';
+
+declare let require: any;
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+  ],
+  providers: [
+    Location,
+    {provide: LocationStrategy, useClass: PathLocationStrategy},
+    LoaderService,
+    StateService,
+    UrlService,
+    LevenshteinDistance
+  ],
+  declarations: [AppComponent, ScenarioComponent, FocusDirective, HighlightSearchMatchPipe],
+  exports: [AppComponent]
+})
+export class PlaygroundCommonModule {
+}
---
+++ core/src/shared/state.service.ts
@@ -0,0 +1,27 @@
+export class StateService {
+  filter: string;
+  filterKey = 'angularPlayground.filter';
+
+  constructor() {
+    this.filter = sessionStorage.getItem(this.filterKey);
+    sessionStorage.removeItem(this.filterKey);
+
+    const beforeUnload = () => {
+      sessionStorage.setItem(this.filterKey, emptyStringIfNull(this.filter));
+      return 'unload';
+    };
+    window.addEventListener('beforeunload', beforeUnload);
+  }
+
+  getFilter() {
+    return this.filter;
+  }
+
+  setFilter(value: string) {
+    this.filter = value;
+  }
+}
+
+function emptyStringIfNull(value: string) {
+  return value ? value : '';
+}
